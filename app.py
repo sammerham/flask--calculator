b12a48b37021922c4e04088ab95250df14500863
Sam Merham <samehisaac@yahoo.com>
2021-04-21 11:51:32 -0700

    completed greet app

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    """Returns a welcome message"""
+    return """
+        <html>
+            <body>
+                <h1>This is our landing page</h1>
+            </body>
+        </html>
+        """
+
+
+
 @app.route('/welcome')
 def welcome():
     """Returns a welcome message"""
@@ -9,6 +22,30 @@ def welcome():
         <html>
             <body>
                 <h1>Welcome</h1>
+            </body>
+        </html>
+        """
+
+
+@app.route('/welcome/home')
+def home():
+    """Returns a welcome message"""
+    return """
+        <html>
+            <body>
+                <h1>Welcome home</h1>
+            </body>
+        </html>
+        """
+
+
+@app.route('/welcome/back')
+def back():
+    """Returns a welcome message"""
+    return """
+        <html>
+            <body>
+                <h1>Welcome back</h1>
             </body>
         </html>
         """
